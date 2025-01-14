5ae0380dc5824291e9ff562722af63cc1cfbcca7
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2025-01-14 16:03:14 +0100

    Update command flags

--- src/commands/upload-results.ts
+++ src/commands/upload-results.ts
@@ -21,49 +21,66 @@ export default class UploadResults extends Command {
   static override examples = ["<%= config.bin %> <%= command.id %>"];
 
   static override flags = {
-    ["client-id"]: Flags.string({
-      description: "the Xray Cloud client ID",
-      helpGroup: "authentication",
-    }),
-    ["client-secret"]: Flags.string({
-      description: "the Xray Cloud client secret",
-      helpGroup: "authentication",
-    }),
     ["csv-file"]: Flags.string({
-      description: "the CSV file which was used for data-driven collection execution",
+      description:
+        "a CSV file which was used for data-driven Bruno execution and will be mapped to Xray's iterations",
+      helpGroup: "TEST EXECUTION ISSUE",
     }),
     ["description"]: Flags.string({
       default: "Generated from Bruno JSON report",
       description: "the description of the test execution issue",
+      helpGroup: "TEST EXECUTION ISSUE",
+    }),
+    ["jira-token"]: Flags.string({
+      dependsOn: ["url"],
+      description: "the Jira API token",
+      env: "JIRA_TOKEN",
+      helpGroup: "AUTHENTICATION",
+    }),
+    ["jira-url"]: Flags.string({
+      description: "the Jira URL",
+      required: true,
     }),
     ["project-key"]: Flags.string({
-      description: "the Jira project key where the test execution issue will be created",
+      description: "the Jira project key where new test execution issues will be created",
       required: true,
     }),
     ["summary"]: Flags.string({
       default: "Bruno test execution",
       description: "the summary of the test execution issue",
+      helpGroup: "TEST EXECUTION ISSUE",
     }),
     ["test-execution"]: Flags.string({
-      description: "the Jira test execution issue to upload the test results to",
+      description: "an existing Jira test execution issue to upload the test results to",
+      helpGroup: "TEST EXECUTION ISSUE",
     }),
-    ["token"]: Flags.string({ description: "the Jira API token", helpGroup: "authentication" }),
-    ["url"]: Flags.string({ description: "the Jira Server/DC URL" }),
+    ["xray-client-id"]: Flags.string({
+      dependsOn: ["xray-client-secret"],
+      description: "the Xray Cloud client ID",
+      env: "XRAY_CLIENT_ID",
+      helpGroup: "AUTHENTICATION",
+    }),
+    ["xray-client-secret"]: Flags.string({
+      dependsOn: ["xray-client-id"],
+      description: "the Xray Cloud client secret",
+      env: "XRAY_CLIENT_SECRET",
+      helpGroup: "AUTHENTICATION",
+    }),
   };
 
   public async run(): Promise<void> {
     const {
       args: { results },
       flags: {
-        "client-id": clientId,
-        "client-secret": clientSecret,
         "csv-file": csvFile,
         description,
+        "jira-token": jiraToken,
+        "jira-url": jiraUrl,
         "project-key": projectKey,
         summary,
         "test-execution": testExecution,
-        token,
-        url,
+        "xray-client-id": xrayClientId,
+        "xray-client-secret": xrayClientSecret,
       },
     } = await this.parse(UploadResults);
 
@@ -72,12 +89,12 @@ export default class UploadResults extends Command {
     ) as BrunoIteration[];
 
     // Choose Xray server or cloud depending on the provided option combinations.
-    const isCloudProject = clientId !== undefined && clientSecret !== undefined;
-    const isServerProject = token !== undefined && url !== undefined;
+    const isCloudProject = xrayClientId !== undefined && xrayClientSecret !== undefined;
+    const isServerProject = jiraToken !== undefined;
 
     if (!isCloudProject && !isServerProject) {
       throw new Error(
-        `One of [--client-id ... --client-secret ...] or [--token ... --url ...] must be provided`
+        `One of [--xray-client-id ... --xray-client-secret ...] or [--jira-token ... --jira-url ...] must be provided`
       );
     }
 
@@ -107,13 +124,13 @@ export default class UploadResults extends Command {
       response = await importExecution(
         `https://xray.cloud.getxray.app/api/v2/import/execution?projectKey=${projectKey}`,
         xrayResults,
-        { clientId, clientSecret }
+        { clientId: xrayClientId, clientSecret: xrayClientSecret }
       );
     } else if (isServerProject) {
       response = await importExecution(
-        `${url}/rest/raven/1.0/import/execution?projectKey=${projectKey}`,
+        `${jiraUrl}/rest/raven/1.0/import/execution?projectKey=${projectKey}`,
         xrayResults,
-        { token }
+        { token: jiraToken }
       );
     }
 
