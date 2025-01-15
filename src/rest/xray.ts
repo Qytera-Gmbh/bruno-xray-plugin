f4ce2e447dae87e8a54273d5c6ef82ab48a93d26
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2025-01-15 17:06:26 +0100

    Rework Xray client

--- src/rest/xray.ts
+++ src/rest/xray.ts
@@ -4,21 +4,7 @@ interface ImportResponse {
   key: string;
 }
 
-/**
- * Uploads test results to the Xray instance.
- *
- * @param url the Xray URL
- * @param results the test results
- * @param credentials the credentials to use
- * @returns the import response
- *
- * @see https://docs.getxray.app/display/XRAY/Import+Execution+Results+-+REST#ImportExecutionResultsREST-XrayJSONresults
- * @see https://docs.getxray.app/display/XRAYCLOUD/Import+Execution+Results+-+REST+v2
- */
-export async function importExecution(
-  url: string,
-  results: XrayTestExecutionResults,
-  credentials:
+type XrayCredentials =
     | {
         /**
          * The Jira server PAT.
@@ -34,25 +20,50 @@ export async function importExecution(
          * The Xray cloud client secret.
          */
         clientSecret: string;
-      }
-): Promise<ImportResponse> {
-  let authorizationValue: string;
-  if ("token" in credentials) {
-    authorizationValue = `Bearer ${credentials.token}`;
-  } else {
-    const token = await fetch("https://xray.cloud.getxray.app/api/v2/authenticate", {
-      body: JSON.stringify({
-        ["client_id"]: credentials.clientId,
-        ["client_secret"]: credentials.clientSecret,
-      }),
-      headers: {
-        ["Accept"]: "application/json",
-        ["Content-Type"]: "application/json",
-      },
-      method: "POST",
-    });
-    authorizationValue = `Bearer ${(await token.json()) as string}`;
+    };
+
+export class XrayClient {
+  private readonly url: string;
+  private readonly isCloudClient: boolean;
+  private readonly credentials: XrayCredentials;
+
+  constructor(
+    config: { baseUrl: string; token: string } | { clientId: string; clientSecret: string }
+  ) {
+    if ("token" in config) {
+      this.url = config.baseUrl;
+      this.credentials = {
+        token: config.token,
+      };
+      this.isCloudClient = false;
+    } else {
+      this.url = "https://xray.cloud.getxray.app";
+      this.credentials = {
+        clientId: config.clientId,
+        clientSecret: config.clientSecret,
+      };
+      this.isCloudClient = true;
+    }
   }
+
+  /**
+   * Uploads test results to the Xray instance.
+   *
+   * @param results the test results
+   * @param projectKey the key of the project to use for new test executions
+   * @returns the import response
+   *
+   * @see https://docs.getxray.app/display/XRAY/Import+Execution+Results+-+REST#ImportExecutionResultsREST-XrayJSONresults
+   * @see https://docs.getxray.app/display/XRAYCLOUD/Import+Execution+Results+-+REST+v2
+   */
+  public async importExecution(
+    results: XrayTestExecutionResults,
+    projectKey: string
+  ): Promise<ImportResponse> {
+    const authorizationValue = await this.getAuthorizationHeader(this.credentials);
+    const url = this.isCloudClient
+      ? `${this.url}/api/v2/import/execution?projectKey=${projectKey}`
+      : `${this.url}/rest/raven/1.0/import/execution?projectKey=${projectKey}`;
   const response = await fetch(url, {
     body: JSON.stringify(results),
     headers: {
@@ -68,4 +79,23 @@ export async function importExecution(
   throw new Error(
     `Unexpected response status ${response.status.toString()}: ${await response.text()}`
   );
+}
+  private async getAuthorizationHeader(credentials: XrayCredentials) {
+    if ("token" in credentials) {
+      return `Bearer ${credentials.token}`;
+    } else {
+      const token = await fetch(`${this.url}/api/v2/authenticate`, {
+        body: JSON.stringify({
+          ["client_id"]: credentials.clientId,
+          ["client_secret"]: credentials.clientSecret,
+        }),
+        headers: {
+          ["Accept"]: "application/json",
+          ["Content-Type"]: "application/json",
+        },
+        method: "POST",
+      });
+      return `Bearer ${(await token.json()) as string}`;
+    }
+  }
 }
